8b5d1c9fd62c4358ac86d585a75fa8efed7ae84f
piyush <soneypiyush@gmail.com>
2021-08-09 22:20:40 +0530

    commit 9/8/2021 22:20

--- app/src/main/java/com/example/loginactivity/BarchartActivity.java
+++ app/src/main/java/com/example/loginactivity/BarchartActivity.java
@@ -10,5 +10,8 @@ public class BarchartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barchart);
+
+        Bundle extras = getIntent().getExtras();
+        int arrayB = extras.getInt("array");
     }
 }
