670c4c0873de65106f2a6e52dd5198d0d7af4ec3
piyush <soneypiyush@gmail.com>
2021-08-11 18:03:00 +0530

    commit 11/8/2021 18:02

--- app/src/main/java/com/example/loginactivity/CalculationActivity.java
+++ app/src/main/java/com/example/loginactivity/CalculationActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,13 +18,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class CalculationActivity extends AppCompatActivity {
 //            variables declared as per column 1 of calculation table in same sequence
 //    float water,electricity,sanitation,PubHealth,PriHealth,housing,pubMassTrans,priMassTrans,pubEduSyatem,priEduSystem,safety,employment,pubSpace,communityLife,leisure,entertain,network,governance,natEnv,quality;
 
 // Values need to be added in an array. while fetching, create above commented variables
 //    Add the values from Firebase to the variables and pass them to array. Below are dummy values
-  float sanitationAvailable,
+  double sanitationAvailable,
         sanitationExpected,
         cost_health_private_existing,
         cost_health_private_expected,
@@ -42,7 +45,7 @@ public class CalculationActivity extends AppCompatActivity {
         private_health_sys,
         housing;
 
-  float availPolStationVar,
+  double availPolStationVar,
           availPriTransVar,
           availPubTransVar,
           exiEduFacPriVar,
@@ -56,25 +59,27 @@ public class CalculationActivity extends AppCompatActivity {
           expSalVar,
           public_mass_transport,private_mass_transport,public_edu_sys,private_edu_sys,safety_security,emp_opportunity;
 
-  float availPubSpacesVar,
+  double availPubSpacesVar,
              expPubSpacesVar,
             exiNeighVisitsVar,
              expNeighVisitsVar,public_space,community;
 
 
- float   publicFacilitiesAvailability,
+ double   publicFacilitiesAvailability,
          publicFacilitiesExpected,
          publicEntertainmentUtilitiesAvailability,
          publicEntertaimnentUtilitiesExpected,
          networkSpeedAvailable,
          networkSpeedExpected,leisure_recreation,network_connectivity,entertainment;
 
-  float  exiQualityVar,
+  double  exiQualityVar,
             expQualityVar,
          exiGovtRespTimeVar,
           expGovtRespTimeVar,
           availNatEnvVar,
           expNatEnvVar,governance,natural_env,quality_of_life;
+
+    double arr[]=new double[20];
 
   String Result;
 
@@ -101,21 +106,23 @@ public class CalculationActivity extends AppCompatActivity {
 
         SharedPreferences preferences=getSharedPreferences("userID", MODE_PRIVATE);
         String userID = preferences.getString("userID", "");
-        Query checkuser = reference.orderByChild("id").equalTo(userID);
+        Query checkuser = reference.orderByChild("userid").equalTo(userID);
 
         checkuser.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if(snapshot.exists()){
                      waterAvailability = snapshot.child(userID).child("section1").child("waterAvailability").getValue(Float.class);
+//                    waterAvailability=50;
                      waterExpected = snapshot.child(userID).child("section1").child("waterExpected").getValue(Float.class);
-                     electricityAvailability = snapshot.child(userID).child("section1").child("electricityAvailability").getValue(Float.class);
+//                    waterExpected=60;
+                    electricityAvailability = snapshot.child(userID).child("section1").child("electricityAvailability").getValue(Float.class);
                      electricityExpected = snapshot.child(userID).child("section1").child("electricityExpected").getValue(Float.class);
                      sanitationAvailable = snapshot.child(userID).child("section1").child("sanitationAvailable").getValue(Float.class);
                      sanitationExpected = snapshot.child(userID).child("section1").child("sanitationExpected").getValue(Float.class);
                      cost_health_public_existing = snapshot.child(userID).child("section1").child("cost_health_public_existing").getValue(Float.class);
                      cost_health_public_expected = snapshot.child(userID).child("section1").child("cost_health_public_expected").getValue(Float.class);
-                     cost_health_private_existing = snapshot.child(userID).child("section1").child("waterAvailability").getValue(Float.class);
+                     cost_health_private_existing = snapshot.child(userID).child("section1").child("cost_health_private_existing").getValue(Float.class);
                      cost_health_private_expected = snapshot.child(userID).child("section1").child("cost_health_private_expected").getValue(Float.class);
                      cost_renting_existing = snapshot.child(userID).child("section1").child("cost_renting_existing").getValue(Float.class);
                      cost_renting_expected = snapshot.child(userID).child("section1").child("cost_renting_expected").getValue(Float.class);
@@ -172,6 +179,77 @@ public class CalculationActivity extends AppCompatActivity {
                     natural_env =(availNatEnvVar-expNatEnvVar)/100;
                     quality_of_life = (exiQualityVar-expQualityVar)/100;
 
+                     arr[0] = water;
+                     arr[1]= electricity;
+                    arr[2]= sanitation;
+                    arr[3]= public_health_sys;
+                    arr[4]=private_health_sys;
+                    arr[5]= housing;
+                    arr[6]= public_mass_transport;
+                    arr[7]= private_mass_transport;
+                    arr[8]= public_edu_sys;
+                    arr[9]= private_edu_sys;
+                    arr[10]= safety_security;
+                    arr[11]= emp_opportunity;
+                    arr[12]= public_space;
+                    arr[13]= community;
+                    arr[14]= leisure_recreation;
+                    arr[15]= entertainment;
+                    arr[16]= network_connectivity;
+                    arr[17]= governance;
+                    arr[18]= natural_env;
+                    arr[19]= quality_of_life;
+
+
+
+//        float arr2[] = {0.4f,-0.02f,0.03f,-5,30,-20,-0.15f,0,-0.03f,-0.06f,-0.05f,-150,-0.2f,-0.04f,-0.15f,-0.06f,-0.04f,-0.16f,-0.2f,-0.03f};
+
+                    for(double i : arr)
+                    {
+                        if(i>=0.26)
+                        {
+                            countGreen++;
+                        }else if(i>=-0.25 && i<=0.25)
+                        {
+                            countBlue++;
+                        }else
+                        {
+                            countRed++;
+                        }
+
+                    }
+//                    Toast.makeText(getApplicationContext(), ""+waterAvailability, Toast.LENGTH_SHORT).show();
+                    for(int i=0;i< arr.length;i++){
+//                        Toast.makeText(getApplicationContext(), ""+arr[i], Toast.LENGTH_SHORT).show();
+                        Log.e("value",""+arr[i]);
+                    }
+
+//        Toast.makeText(getApplicationContext(), ""+arr[0], Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(), list.get(0).toString(), Toast.LENGTH_SHORT).show();
+
+                    if(countGreen>16)
+                    {
+                        Toast.makeText(getApplicationContext(), "Dark Green", Toast.LENGTH_SHORT).show();
+                        Result = "dark";
+                    }else if( (countGreen +countBlue)>=13 || (countGreen +countBlue)<=16)
+                    {
+                        Toast.makeText(getApplicationContext(), "Light Green", Toast.LENGTH_SHORT).show();
+                        Result = "green";
+                    }else if(((countGreen +countBlue)>=8) || ((countGreen +countBlue)<=12))
+                    {
+                        Toast.makeText(getApplicationContext(), "yellow", Toast.LENGTH_SHORT).show();
+                        Result = "yellow";
+                    }else if(((countGreen +countBlue)>=4) || ((countGreen +countBlue)<=7))
+                    {
+                        Toast.makeText(getApplicationContext(), "orange", Toast.LENGTH_SHORT).show();
+                        Result = "orange";
+                    }else
+                    {
+                        Toast.makeText(getApplicationContext(), "red", Toast.LENGTH_SHORT).show();
+                        Result = "Red";
+                    }
+
+
 
 
                 }
@@ -183,64 +261,21 @@ public class CalculationActivity extends AppCompatActivity {
             }
         });
 
-        float arr[] = {water,electricity,sanitation,public_health_sys,private_health_sys,housing,
-                public_mass_transport,private_mass_transport,public_edu_sys,private_edu_sys,safety_security,emp_opportunity,
-                public_space,community,leisure_recreation,entertainment,network_connectivity,governance,natural_env,quality_of_life};
-
-        float arr2[] = {0.4f,-0.02f,0.03f,-5,30,-20,-0.15f,0,-0.03f,-0.06f,-0.05f,-150,-0.2f,-0.04f,-0.15f,-0.06f,-0.04f,-0.16f,-0.2f,-0.03f};
-
-        for(float i : arr)
-        {
-            if(i>=0.26)
-            {
-                countGreen++;
-            }else if(i>=-0.25 && i<=0.25)
-            {
-                countBlue++;
-            }else
-            {
-                countRed++;
-            }
-
-        }
-
-
-        if(countGreen>16)
-        {
-            Toast.makeText(getApplicationContext(), "Dark Green", Toast.LENGTH_SHORT).show();
-            Result = "dark";
-        }else if( (countGreen +countBlue)>=13 || (countGreen +countBlue)<=16)
-        {
-            Toast.makeText(getApplicationContext(), "Light Green", Toast.LENGTH_SHORT).show();
-            Result = "green";
-        }else if(((countGreen +countBlue)>=8) || ((countGreen +countBlue)<=12))
-        {
-            Toast.makeText(getApplicationContext(), "yellow", Toast.LENGTH_SHORT).show();
-            Result = "yellow";
-        }else if(((countGreen +countBlue)>=4) || ((countGreen +countBlue)<=7))
-        {
-            Toast.makeText(getApplicationContext(), "orange", Toast.LENGTH_SHORT).show();
-            Result = "orange";
-        }else
-        {
-            Toast.makeText(getApplicationContext(), "red", Toast.LENGTH_SHORT).show();
-            Result = "Red";
-        }
 
 
 
         goToMaps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences sh = getSharedPreferences("MySharedPref2",MODE_PRIVATE);
-                String id = sh.getString("Id", "");
+                SharedPreferences preferences=getSharedPreferences("userID", MODE_PRIVATE);
+                String userID = preferences.getString("userID", "");
                 RootNode = FirebaseDatabase.getInstance();//gets all the elements in db from that select 1 element from tree struc
                 reference = RootNode.getReference("users");
-                reference.child(id).child("result").setValue(Result);
+                reference.child(userID).child("result").setValue(Result);
+                Toast.makeText(getApplicationContext(), Result, Toast.LENGTH_SHORT).show();
 
-
-                Intent intent=new Intent(getApplicationContext(),MapsActivity.class);
-                startActivity(intent);
+//                Intent intent=new Intent(getApplicationContext(),MapsActivity.class);
+//                startActivity(intent);
             }
         });
 
@@ -248,7 +283,7 @@ public class CalculationActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(),BarchartActivity.class);
-                intent.putExtra("array",arr2);
+                intent.putExtra("array",arr);
                 startActivity(intent);
             }
         });
